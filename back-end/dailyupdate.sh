0849f04dfe9a35f89be225218f313bef5f434d62
MarkGrimwood <83716751+MarkGrimwood@users.noreply.github.com>
2021-05-16 21:11:37 +0100

    Update dailyupdate.sh

--- back-end/dailyupdate.sh
+++ back-end/dailyupdate.sh
@@ -3,10 +3,3 @@
 # Update the timing
 cd /home/pi/Mognet-All-Sky-Camera/back-end/
 sudo ./mgasc.sh>update.log
-
-# Ensure we're up to date
-sudo apt-get -y update>>update.log
-sudo apt-get -y upgrade>>update.log
-sudo apt-get clean>>update.log
-sudo apt-get autoclean>>update.log
-sudo apt-get autoremove>>update.log
