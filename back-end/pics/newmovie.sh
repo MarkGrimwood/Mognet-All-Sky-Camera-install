af33b0388557872af7e05f507768a814e77935c0
MarkGrimwood <83716751+MarkGrimwood@users.noreply.github.com>
2021-06-16 10:49:13 +0100

    Update newmovie.sh

--- back-end/pics/newmovie.sh
+++ back-end/pics/newmovie.sh
@@ -97,4 +97,4 @@ sudo chown nobody "$WEBPATH/$PERIOD/$THISMOVIE"
 
 # Clean up ready for next time
 mv -f "$WORKPATH/$WEBCAMPD.jpg" "$WEBPATH/$PERIOD/"
-rm -f "$WORKPATH/webcam$PERIOD*.jpg"
+rm -f "$WORKPATH/$WEBCAMPD-*.jpg"
