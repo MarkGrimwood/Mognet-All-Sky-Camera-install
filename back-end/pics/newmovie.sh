68572502a4b642adde20cce9f2a7a18d1264441f
MarkGrimwood <83716751+MarkGrimwood@users.noreply.github.com>
2021-06-27 17:24:15 +0100

    Update newmovie.sh

--- back-end/pics/newmovie.sh
+++ back-end/pics/newmovie.sh
@@ -29,6 +29,16 @@ CPU_TEMP_FULL=$(vcgencmd measure_temp)
 CPU_TEMP=${CPU_TEMP_FULL:5:${#CPU_TEMP_FULL}-7}${CPU_TEMP_FULL: -1}
 IMAGE_TEXT="$HUMANDATE : CPU Temp $CPU_TEMP"
 
+# Wait until the capture script has finished. And don't clash with another instance of this script
+COUNTCAPTURE=$(ps -ef | grep capture | grep bash | wc -l)
+COUNTNEW=$(ps -ef | grep newmovie | grep bash | wc -l)
+while [ true ]; do
+  if [ "$COUNTCAPTURE" -eq 0 ] && [ "$COUNTNEW" -le 1 ]
+    break;
+  fi
+  sleep 1s
+done
+
 # Archive last day's files if they exist
 if [ -d "$WEBPATH/$PERIOD" ]; then
   echo "$WEBPATH/$PERIOD exists"
