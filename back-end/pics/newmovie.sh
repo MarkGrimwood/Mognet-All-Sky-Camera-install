49246e58e39342990ab587bd9e41b9c93c1c5cc7
MarkGrimwood <83716751+MarkGrimwood@users.noreply.github.com>
2024-01-11 20:02:34 +0000

    Update newmovie.sh

--- back-end/pics/newmovie.sh
+++ back-end/pics/newmovie.sh
@@ -83,10 +83,12 @@ done
 echo "Initial capture"
 if [ "$PERIOD" == "day" ]; then
   # Capture the day image
-  raspistill -ISO auto -awb greyworld --nopreview --exposure auto -w 1440 -h 1080 -o "$STANDARDCAPTURE"
+#  raspistill -ISO auto -awb greyworld --nopreview --exposure auto -w 1440 -h 1080 -o "$STANDARDCAPTURE
+  libcamera-still --autofocus-mode manual --lens-position 0.0 --nopreview --exposure normal --width 1440 --height 1080 -o "$STANDARDCAPTURE"
 else
   # Capture the night image. Although set to 10 seconds it takes closer to 20 on the Pi Zero
-  raspistill -ISO auto -awb greyworld --nopreview --exposure off --stats -w 1440 -h 1080 --contrast 20 -ag 12.0 -dg 2.0 -ss 10000000 -o "$STANDARDCAPTURE"
+#  raspistill -ISO auto -awb greyworld --nopreview --exposure off --stats -w 1440 -h 1080 --contrast 20 -ag 12.0 -dg 2.0 -ss 10000000 -o "$STANDARDCAPTURE"
+  libcamera-still --autofocus-mode manual --lens-position 0.0 --nopreview --exposure normal --width 1440 --height 1080 --contrast 20 --gain 20.0 --shutter 10000000 --awbgains 1.1,2.8 --immediate -o "$STANDARDCAPTURE"
 fi
 
 # Stamp the image with the date and time and put it into the web day directory along with the thumbnail
@@ -111,7 +113,7 @@ sudo rm -f $WORKPATH/$THISMOVIE
 
 # Make a new movie with these captures
 echo "Creating movie"
-ffmpeg -y -framerate 20 -pix_fmt yuv420p -pattern_type glob -i "$WORKPATH/webcam$PERIOD*.jpg" -c:v libx264 "$WORKPATH/$THISMOVIE"
+ffmpeg -y -framerate 10 -pix_fmt yuv420p -pattern_type glob -i "$WORKPATH/webcam$PERIOD*.jpg" -c:v libx264 "$WORKPATH/$THISMOVIE"
 
 # Copy for web display
 cp -f "$WORKPATH/$THISMOVIE" "$WEBPATH/$PERIOD/$THISMOVIE"
