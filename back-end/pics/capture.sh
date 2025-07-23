842da1cbf0eb5a17f97afcb6af0a4b96bae1b505
MarkGrimwood <83716751+MarkGrimwood@users.noreply.github.com>
2025-07-23 23:26:42 +0100

    Update capture.sh

--- back-end/pics/capture.sh
+++ back-end/pics/capture.sh
@@ -44,10 +44,10 @@ then
 
   if [ "$PERIOD" == "day" ]; then
     # Capture the day image
-    libcamera-still --autofocus-mode manual --lens-position 0.0 --nopreview --exposure normal --width 1440 --height 1080 -o "$STANDARDCAPTURE" 
+    rpicam-still --autofocus-mode manual --lens-position 0.0 --nopreview --exposure normal --width 1440 --height 1080 -o "$STANDARDCAPTURE" 
  else
     # Capture the night image. Although set to 10 seconds it takes closer to 20 on the Pi Zero
-    libcamera-still --autofocus-mode manual --lens-position 0.0 --nopreview --exposure normal --width 1440 --height 1080 --contrast 20 --gain 20.0 --shutter 10000000 --awbgains 1.1,2.8 --immediate -o "$STANDARDCAPTURE"
+    rpicam-still --autofocus-mode manual --lens-position 0.0 --nopreview --exposure normal --width 1440 --height 1080 --contrast 20 --gain 20.0 --shutter 10000000 --awbgains 1.1,2.8 --immediate -o "$STANDARDCAPTURE"
   fi
 
   # Add date and time stamp
